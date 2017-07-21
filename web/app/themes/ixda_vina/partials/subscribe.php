b4d886a11f8532f80162ac0ab70dc6701e37b145
Felipe Lavín Z <felipe@bloom-ux.com>
2017-07-21 13:30:42 -0400

    integracion formulario mailchimp

--- web/app/themes/ixda_vina/partials/subscribe.php
+++ web/app/themes/ixda_vina/partials/subscribe.php
@@ -4,14 +4,14 @@
 			<h2>Suscríbete</h2>
 			<p>Entérate de nuestros próximos eventos y seminarios y participa junto a nuestro equipo.</p>
 		</header>
-		<form class="row">
+		<form class="row" action="//ixda.us16.list-manage.com/subscribe/post?u=7ad70c88f30a5d7ba8d8f1e98&amp;id=60807c6143" method="post" name="mc-embedded-subscribe-form" target="_blank">
 			<div class="col-sm-4">
 				<label for="subscribe__name">Nombre:</label>
-				<input type="text" name="name" id="subscribe__name" class="form-control">
+				<input type="text" name="FULLNAME" id="subscribe__name" class="form-control">
 			</div>
 			<div class="col-sm-4">
 				<label for="subscribe__email">Correo electrónico:</label>
-				<input type="text" name="email" id="subscribe__email" class="form-control">
+				<input type="text" name="EMAIL" id="subscribe__email" class="form-control">
 			</div>
 			<div class="col-sm-3">
 				<input type="submit" value="Enviar" class="subscribe__submit">
